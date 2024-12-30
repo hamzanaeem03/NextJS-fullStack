4183ff60c992cdafff52d61c0b8167a7103880b6
hamzanaeem03 <hamza04032003naeem@gmail.com>
2024-12-30 22:01:56 +0500

    form vadation in popup

--- src/app/components/BookingDialog.jsx
+++ src/app/components/BookingDialog.jsx
@@ -10,6 +10,27 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Patient name is required." }),
+  phone: z.string().min(10, { message: "Phone number must be at least 10 digits." }),
+  gender: z.string(),
+  experience: z.number().min(0, { message: "Experience must be a positive number." }),
+})
 
 export function BookingDialog() {
   return (
@@ -21,21 +42,21 @@ export function BookingDialog() {
         <DialogHeader>
           <DialogTitle>Book Appiontment</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Enter details of patient.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="name" className="text-right">
-              Name
+            Patient Name
             </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            <Input id="name"  className="col-span-3" />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
+            <Label htmlFor="age" className="text-right">
+              Age
             </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
+            <Input id="age" type="number" className="col-span-3" />
           </div>
         </div>
         <DialogFooter>
